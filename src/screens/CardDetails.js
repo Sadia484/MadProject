52b5ceb00f1d33f0c332dcfb7ade74de72c6a81e
Abstract Programmer <AbstractProgrammer@example.com>
2024-01-02 00:20:27 +0500

    second

---
+++ src/screens/CardDetails.js
@@ -0,0 +1,110 @@
+import React from 'react';
+import { View, Text, Image, ScrollView, StyleSheet, FlatList } from 'react-native';
+
+const CardDetails = ({ route }) => {
+  const apiKey = 'AIzaSyBaXNfik0Mi1P24_467aCFoak_7PWWjvQY'; 
+
+  const { restaurant, details } = route.params;
+
+  return (
+    <ScrollView style={styles.container}>
+      <Image
+        source={{
+          uri: restaurant.photos && restaurant.photos.length > 0
+            ? `https://maps.googleapis.com/maps/api/place/photo?maxwidth=800&photoreference=${restaurant.photos[0].photo_reference}&key=${apiKey}`
+            : null,
+        }}
+        style={styles.restaurantImage}
+      />
+      <View style={styles.detailsContainer}>
+        <Text style={styles.restaurantName}>{restaurant.name}</Text>
+        <Text style={styles.restaurantRating}>
+          Rating: {restaurant.rating} ({restaurant.user_ratings_total} reviews)
+        </Text>
+        <Text style={styles.restaurantDescription}>{restaurant.vicinity}</Text>
+        <Text style={styles.sectionHeader}>Location:</Text>
+        <Text style={styles.sectionText}>{details && details.formatted_address}</Text>
+        <Text style={styles.sectionHeader}>Reviews:</Text>
+        {details && details.reviews ? (
+          details.reviews.map((review, index) => (
+            <View key={index} style={styles.reviewContainer}>
+              <Text style={styles.reviewAuthor}>{review.author_name}</Text>
+              <Text style={styles.reviewText}>{review.text}</Text>
+            </View>
+          ))
+        ) : (
+          <Text>No reviews available</Text>
+        )}
+        <Text style={styles.sectionHeader}></Text>
+        <FlatList
+          horizontal
+          data={restaurant.photos && restaurant.photos.slice(1, 6)} // Show up to 5 additional photos
+          keyExtractor={(item, index) => index.toString()}
+          renderItem={({ item }) => (
+            <Image
+              source={{
+                uri: `https://maps.googleapis.com/maps/api/place/photo?maxwidth=800&photoreference=${item.photo_reference}&key=${apiKey}`,
+              }}
+              style={styles.additionalImage}
+            />
+          )}
+        />
+      </View>
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+    },
+    restaurantImage: {
+      width: '100%',
+      height: 200,
+    },
+    detailsContainer: {
+      padding: 16,
+    },
+    restaurantName: {
+      fontSize: 24,
+      fontWeight: 'bold',
+      marginBottom: 8,
+    },
+    restaurantRating: {
+      fontWeight: 'bold',
+      fontSize: 18,
+      marginBottom: 8,
+    },
+    restaurantDescription: {
+      fontSize: 16,
+      marginBottom: 16,
+    },
+    sectionHeader: {
+      fontSize: 20,
+      fontWeight: 'bold',
+      marginTop: 16,
+      marginBottom: 8,
+    },
+    sectionText: {
+      fontSize: 16,
+      marginBottom: 16,
+    },
+    reviewContainer: {
+      marginBottom: 16,
+    },
+    reviewAuthor: {
+      fontSize: 18,
+      fontWeight: 'bold',
+    },
+    reviewText: {
+      fontSize: 16,
+    },
+    additionalImage: {
+      width: 200,
+      height: 120,
+      marginRight: 8,
+      borderRadius: 8,
+    },
+  });
+  
+  export default CardDetails;
